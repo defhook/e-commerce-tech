2fd8437ca9038b93a87e15e1d0133bba24cc874f
defhook <defhook152@gmail.com>
2022-05-01 13:35:46 -0700

    added models and routes

--- Develop/models/index.js
+++ Develop/models/index.js
@@ -4,13 +4,25 @@ const Category = require('./Category');
 const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
-// Products belongsTo Category
+// Products belongsTo Category ex: [Addidas Shoes belong to Shoe Category,  1Product:1Category]
+Product.belongsTo(Category, {
+  foreignKey: 'category_id'
+})
+// Categories have many Products ex [ Jacket Category => Summer ,spring ,winter 1 category: M Products ]
+Category.hasMany(Product, {
+  foreignKey: 'category_id'
+}); 
 
-// Categories have many Products
-
-// Products belongToMany Tags (through ProductTag)
-
+// Products belongToMany Tags (through ProductTag) M : M [4shoes: 4tags]
+Product.belongsToMany(Tag, {
+  through: ProductTag, 
+  foreignKey: 'product_id'
+});
 // Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(Product, {
+  through: ProductTag, 
+  foreignKey: 'tag_id'
+}); 
 
 module.exports = {
   Product,
